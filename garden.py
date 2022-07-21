81c29337a790ca975b785cdbc76ff7d2240cdfd1
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-21 15:32:59 -0400

    Update garden.py

--- garden.py
+++ garden.py
@@ -1,2 +1,14 @@
 from random import randint
 import time
+WIDTH = 800
+HEIGHT = 600
+
+game_over = False
+finalized = False
+garden_happy = True
+fangflower_collision = False
+
+time_elapsed = 0
+start_time = 0
+start_time = time.time()
+
